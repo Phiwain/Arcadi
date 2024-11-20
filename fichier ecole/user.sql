708cebe6c165d219c64749773b416188ae307f4c
Phiwain <84239854+Phiwain@users.noreply.github.com>
2024-11-20 16:04:14 +0100

    Update user.sql

--- fichier ecole/user.sql
+++ fichier ecole/user.sql
@@ -2,6 +2,10 @@
 
 INSERT INTO `user` (password, email, roles, nom, prenom) VALUES
 -- Administrateur
-('$2y$13$6NzlXwguj6gSQwxt1jOI6O28Q2LVc/siVk8nCf9RDE4GUCP2DLVny', 'admin@acardia.fr', '["ROLE_ADMIN"]', 'admin', 'admin'),
+('$2y$13$6NzlXwguj6gSQwxt1jOI6O28Q2LVc/siVk8nCf9RDE4GUCP2DLVny', 'admin@zooacardia.fr', '["ROLE_ADMIN"]', 'admin', 'admin'),
+-- Employée
+('$2y$13$6NzlXwguj6gSQwxt1jOI6O28Q2LVc/siVk8nCf9RDE4GUCP2DLVny', 'employee@zooacardia.fr', '["ROLE_EMPLOYEE"]', 'employee', 'employee'),
+-- Veterinaire
+('$2y$13$6NzlXwguj6gSQwxt1jOI6O28Q2LVc/siVk8nCf9RDE4GUCP2DLVny', 'veterinaire@zooacardia.fr', '["ROLE_VETERINAIRE"]', 'veterinaire', 'veterinaire'),
 
 
