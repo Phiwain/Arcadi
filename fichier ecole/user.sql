b79bcf375053b3d9d3f3365638df09bc2d37b809
macbook <seb5554@hotmail.fr>
2024-11-20 16:00:04 +0100

    ajout fichier ecoles

---
+++ fichier ecole/user.sql
@@ -0,0 +1,7 @@
+-- Insertion des utilisateurs dans la table `user`
+
+INSERT INTO `user` (password, email, roles, nom, prenom) VALUES
+-- Administrateur
+('$2y$13$6NzlXwguj6gSQwxt1jOI6O28Q2LVc/siVk8nCf9RDE4GUCP2DLVny', 'admin@acardia.fr', '["ROLE_ADMIN"]', 'admin', 'admin'),
+
+
