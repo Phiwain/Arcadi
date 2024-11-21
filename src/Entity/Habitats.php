b3934ebdc6d2fa8c7fc710f564bedae35c706be8
Phiwain <84239854+Phiwain@users.noreply.github.com>
2024-11-21 15:47:27 +0100

    Update Habitats.php
    bug

--- src/Entity/Habitats.php
+++ src/Entity/Habitats.php
@@ -18,7 +18,7 @@ class Habitats
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
